ce31a2fcf747309dcefce85b506a694b7fe73406
Marquis <theusual@icloud.com>
2017-07-26 15:34:53 -0600

    Restructuring
    condensed CSS files to one Sass document, added javascript buttons for
    each page, reorganized file structure

---
+++ javascript/gal2.js
@@ -0,0 +1,52 @@
+  var psGal = document.getElementById("ps"), illGal = document.getElementById("ill"), appGal = document.getElementById("app");
+
+  var forward = document.getElementById("arrowButtonR"), backward = document.getElementById("arrowButtonL");
+
+var photoShop = ["riverP.jpg", "shepP.png", "montuckyP.png", "leninP.jpg", "nozzleP.jpg", "soundShieldP.jpg", "thrustReverserP.jpg","hogJohnson.jpg"];
+
+var illustrator = ["wagsWagonI.jpg", "theUsualI.jpg", "shepI.png", "muirI.jpg", "hootI.jpg", "hoot7I.jpg", "hoot6I.jpg", "hoot5I.jpg", "hoot4I.png", "hoot3I.jpg", "hoot2I.jpg", "hookSetI.jpg", "genI.jpg", "exploreWinterI.jpg", "exploreSummerI.jpg", "evolutionOfWinterI.jpg", "chinI.jpg", "fishCtI.jpg"];
+
+var apps = ["checkTheFlowsA.png", "importVisA.jpg", "smithA.png", "songMapA.png", "bullTrout.png"];
+
+var placeHolder = ["placeHolder.png"];
+
+var category = placeHolder;
+var iter = 0;
+show();
+
+psGal.onclick = function() {
+    category = photoShop;
+    iter = 0;
+    show();
+}
+
+illGal.onclick = function() {
+    category = illustrator;
+    iter = 0;
+    show();
+}
+
+appGal.onclick = function() {
+    category = apps;
+    iter = 0;
+    show();
+}
+
+backward.onclick = function() {
+    iter --;
+    if (iter<0){
+    iter = category.length-1;
+    }
+    show();
+};
+forward.onclick = function() {
+    iter ++;
+    if (iter>category.length-1){
+    iter = 0;
+    }
+    show();
+};
+function show() {
+    document.getElementById("gallery").src = "photos/" + category[iter];
+    document.getElementById("count").innerHTML = (iter+1) + "/" + category.length;
+}
---
+++ javascript/navButtons.js
@@ -0,0 +1,18 @@
+ function hoverAbout(element) {
+     element.setAttribute('src', 'photos/aboutDown.jpg');
+ }
+ function unhoverAbout(element) {
+     element.setAttribute('src', 'photos/aboutB.jpg');
+ }
+ function hoverGallery(element) {
+     element.setAttribute('src', 'photos/galleryDown.jpg');
+ }
+ function unhoverGallery(element) {
+     element.setAttribute('src', 'photos/galleryB.jpg');
+ }
+ function hoverContact(element) {
+     element.setAttribute('src', 'photos/contactDown.jpg');
+ }
+ function unhoverContact(element) {
+     element.setAttribute('src', 'photos/contactB.jpg');
+ }
